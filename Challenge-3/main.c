da1d308f8d01effc1afc5270d434745b6f6bdc32
Nihar-M <niharmakadia12@gmail.com>
2021-12-02 19:54:46 +1100

    completed challenge 3

---
+++ Challenge-3/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char file[] = "input.txt";
+
+    FILE* fd = fopen(file,"r");
+    if (fd == NULL) {
+        printf("Error occured opening file %s.\n",file );
+        return 1;
+    }
+
+    int depth = 0;
+    int distance = 0;
+
+    int size = 0;
+    char direction[8];
+
+    while (!feof(fd))
+    {
+        if (fscanf(fd,"%s %d ",direction, &size) == 2)
+        {
+            if (strcmp(direction,"forward") == 0)
+                distance += size;
+            else if (strcmp(direction,"up") == 0)
+                depth -= size;
+            else if (strcmp(direction,"down") == 0)
+                depth += size;
+            else
+                printf("Direction not recognised");
+        }
+    }
+
+    fclose(fd);
+    printf("Distance = %d\n", distance);
+    printf("Depth = %d\n", depth);
+    return 0;
+}
