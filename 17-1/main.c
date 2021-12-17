54433fbdda792a061e3b8689a8d88f21ee218031
Nihar-M <niharmakadia12@gmail.com>
2021-12-17 17:47:23 +1100

    completed day 17 both parts

---
+++ 17-1/main.c
@@ -0,0 +1,87 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+int main()
+{
+    char file[] = "input.txt";
+
+    FILE *fd = fopen(file, "r");
+    if (fd == NULL)
+    {
+        printf("Error occured opening file %s.\n", file);
+        return 1;
+    }
+    
+    int xmin;
+    int xmax;
+    int ymin;
+    int ymax;
+
+    fscanf(fd, "target area: x=%d..%d, y=%d..%d", &xmin, &xmax, &ymin, &ymax);
+
+    // printf("target area: x=%d..%d, y=%d..%d\n", xmin, xmax, ymin, ymax);
+
+    int vxmin = -1;
+    int vxmax = -1;
+    for (int i = 0; i < 100; i++)
+    {
+        int tri = (i)*(i+1)/2;
+        if (vxmin == -1){
+            if (xmin <= tri && tri <= xmax) {
+                vxmin = i;
+                vxmax = i;
+            }
+        }
+        else {
+            if (xmin <= tri && tri <= xmax)
+                vxmax = i;
+        }
+    }
+
+    // printf("possible vx: %d-%d\n", vxmin, vxmax);
+
+    int max_y_reached = 0;
+
+    for (int vyi = 0; vyi < 1000; vyi++)
+    {
+        for (int vxi = vxmin; vxi <= vxmax; vxi++)
+        {
+            int x = 0;
+            int y = 0;
+            int vx = vxi;
+            int vy = vyi;
+            int hits = 0;
+            int max_y = 0;
+            while (y >= ymin && x <= xmax)
+            {
+                if (ymin <= y && y <= ymax && xmin <= x && x <= xmax) {
+                    hits = 1;
+                    // printf("(%d,%d) hits and gets max of %d\n", vxi, vyi, max_y);
+                    break;
+                }
+                else {
+                    x += vx;
+                    y += vy;
+                    if (y > max_y)
+                         max_y = y;
+                    vy--;
+                    if (vx > 0) 
+                        vx--;
+                } 
+            }
+            if (hits) {
+                if (max_y > max_y_reached) max_y_reached = max_y;
+            }
+        }
+
+    }    
+
+    printf("Highest Y value: %d\n", max_y_reached);
+
+    fclose(fd);
+
+    return 0;
+}
